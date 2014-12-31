5e935ce436a95b1a5ab4fda45b6e9360e8e8042a
Pu.Guihong <pu.guihong@outlook.com>
2014-12-31 17:40:46 +0800

    test

--- common.spring.mainapp/Program.cs
+++ common.spring.mainapp/Program.cs
@@ -14,10 +14,14 @@ namespace common.spring.mainapp
         {
             try
             {
-                IApplicationContext ctx = ConfigurationSettings.GetConfig("spring/context") as IApplicationContext;
-                //IApplicationContext ctx = System.Configuration.ConfigurationManager.GetSection("spring/context") as IApplicationContext;
+                //IApplicationContext ctx = ConfigurationSettings.GetConfig("spring/context") as IApplicationContext;
+                IApplicationContext ctx = System.Configuration.ConfigurationManager.GetSection("spring/context") as IApplicationContext;
                 common.ICommon.ISayHello sayHello = (common.ICommon.ISayHello)ctx.GetObject("mySayHello");
                 sayHello.SayHelloTo("zhenyulu");
+
+                Spring.Context.IApplicationContext context = Spring.Context.Support.ContextRegistry.GetContext();
+                common.ICommon.ISayHello sayH = context.GetObject() as ISayHello;
+
             }
             catch (Exception)
             {                
