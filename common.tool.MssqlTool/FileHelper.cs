2654c1b487ba9f03da0bef936a0a1166c8b7f93f
Pu.Guihong <pu.guihong@outlook.com>
2015-01-07 10:20:10 +0800

    20150107 添加entity生成方法demo

---
+++ common.tool.MssqlTool/FileHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace common.tool.MssqlTool
+{
+    public class FileHelper
+    {
+        private string _file = "Entitys.txt";
+        private string _data;
+        public FileHelper(string _d)
+        {
+            _data = _d;
+            WriteFile();
+        }
+        public bool WriteFile()
+        {
+            bool _result = false;
+            
+            try
+            {
+                StreamWriter sw = new StreamWriter(_file);
+                sw.WriteLine(_data);
+                _result = true;
+                sw.Close();
+                return _result;
+            }
+            catch (Exception)
+            {
+                return false;
+                throw;
+            }
+        } 
+
+    }
+}
--- common.tool.MssqlTool/Form1.cs
+++ common.tool.MssqlTool/Form1.cs
@@ -34,12 +34,36 @@ namespace common.tool.MssqlTool
             {
                 foreach (DataRow dr in dt.Rows)
                 {
+                    StringPlus(dr[0].ToString(), dr[1].ToString());
                     richTextBox1.AppendText("columns: " + dr[0].ToString() + "          type: " + dr[1].ToString() +"\n");
                 }
             }
 
             ds.Tables[0].Columns[0].ToString();
             return flag;
+        }
+
+        public void StringPlus(string _col_name,string _type)
+        {
+            if (_type == "bigint" || _type == "int" || _type == "money")
+            {
+                _type = "int";
+            }
+            else if (_type == "")
+            {
+                _type = "string";
+            }
+            else
+            {
+                _type = "string";
+            }
+            StringBuilder sb = new StringBuilder();
+            sb.Append("[Description('')] \n");
+            sb.Append("[DataCloumn('" + _col_name + "')] \n");
+            sb.Append("[LogProp(LogPropType.Must) \n]");
+            sb.Append("public "+_type+" "+_col_name + "{ get; set; }");
+
+            FileHelper _file = new FileHelper(sb.ToString());
         }
     }
 }
